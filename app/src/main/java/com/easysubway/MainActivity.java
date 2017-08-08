e62e0fbbf2aaa3dd4a82946f29ce015ccd44ea1d
eunhye Lee <ehlee@soongsil.ac.kr>
2017-08-08 14:31:04 +0900

    base application

--- app/src/main/java/com/easysubway/MainActivity.java
+++ app/src/main/java/com/easysubway/MainActivity.java
@@ -1,13 +1,66 @@
 package com.easysubway;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import kr.go.seoul.trafficsubway.Common.BaseActivity;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+public class MainActivity extends BaseActivity {
+
+    public MainActivity()
+    {
+        openAPIKey = "70575677706d696333327152507752";
+        subwayLocationAPIKey = "48435751656d696335367153677846";
+    }
+
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(getIntent() != null && getIntent().getStringExtra("OpenAPIKey") != null)
+            openAPIKey = getIntent().getStringExtra("OpenAPIKey");
+        if(getIntent() != null && getIntent().getStringExtra("SubwayLocationAPIKey") != null)
+            subwayLocationAPIKey = getIntent().getStringExtra("SubwayLocationAPIKey");
+        initView();
     }
+
+    private void initView()
+    {
+        btnBackSubway = (ImageView)findViewById(R.id.btn_back_subway);
+        btnBackSubway.setOnClickListener(new android.view.View.OnClickListener()
+                                         {
+
+                                             public void onClick(View view)
+                                             {
+                                                 finish();
+                                             }
+
+                                             final MainActivity this$0;
+
+                                             {
+                                                 this.this$0 = MainActivity.this;
+                                                 // super();
+                                             }
+                                         }
+        );
+        lineMapWebview = (WebView)findViewById(R.id.line_map_webview);
+        lineMapWebview.setWebViewClient(new WebViewClient());
+        lineMapWebview.getSettings().setJavaScriptEnabled(true);
+        lineMapWebview.getSettings().setBuiltInZoomControls(true);
+        lineMapWebview.getSettings().setSupportZoom(true);
+        lineMapWebview.getSettings().setDisplayZoomControls(false);
+        lineMapWebview.getSettings().setDefaultTextEncodingName("UTF-8");
+        mWebViewInterface = new WebViewInterface(this, lineMapWebview, openAPIKey, subwayLocationAPIKey);
+        lineMapWebview.addJavascriptInterface(mWebViewInterface, "Android");
+        lineMapWebview.loadUrl("file:///android_asset/mSeoul_Subway.html");
+    }
+
+    private String openAPIKey;
+    private String subwayLocationAPIKey;
+    private ImageView btnBackSubway;
+    private WebView lineMapWebview;
+    private WebViewInterface mWebViewInterface;
 }
